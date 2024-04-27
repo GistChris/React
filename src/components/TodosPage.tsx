857e51ea3e79a244532392d84ffaa99aa5140806
Alexander Chentsov <2262659@cshawi.ca>
2024-04-27 12:19:19 -0400

    branch alex

--- src/components/TodosPage.tsx
+++ src/components/TodosPage.tsx
@@ -1,32 +1,33 @@
-import React,{FC,useEffect,useState} from 'react';
+import React, { FC, useEffect, useState } from "react";
 import { ITodo, IUser } from "../types/types";
 import List from "../components/List";
 import TodoItem from "../components/TodoItem";
 import axios from "axios";
-const TodosPage:FC = () => {
-    const [todo, setTodos] = useState<ITodo[]>([]);
-    useEffect(() => {   
-      fetchTodos();
-    }, []);
-  
-    async function fetchTodos() {
-      try {
-        const response = await axios.get<ITodo[]>(
-          "https://jsonplaceholder.typicode.com/todos?_limit=10"
-        );
-        setTodos(response.data);
-        // console.log(response.data[0].name);
-        // console.log(users);
-      } catch (e) {
-        alert(e);
-      }
+const TodosPage: FC = () => {
+  const [todo, setTodos] = useState<ITodo[]>([]);
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+      );
+      setTodos(response.data);
+      // console.log(response.data[0].name);
+      // console.log(users);
+      // console.log(users);
+    } catch (e) {
+      alert(e);
     }
-    return (
-        <List
-        items={todo}
-        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
-      />
-    );
+  }
+  return (
+    <List
+      items={todo}
+      renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+    />
+  );
 };
 
 export default TodosPage;
